6dff059e1c1c51aaa6f4798a8e1134aec50488bc
Jxckson-ctrl <84982387+Jxckson-ctrl@users.noreply.github.com>
2021-09-21 13:40:49 -0500

    Finished product.

---
+++ README.md
@@ -0,0 +1,60 @@
+Aether [![NPM version](https://img.shields.io/npm/v/eris.svg?style=flat-square&color=informational)](https://npmjs.com/package/eris)
+====
+
+A Node.js wrapper for interfacing with Discord.
+
+Installing
+----------
+
+You will need NodeJS 10.4+.
+
+```
+npm install --no-optional Aether
+```
+
+If you need voice support, remove the `--no-optional`.
+
+Ping Pong Example
+-----------------
+
+```js
+const Aether = require("Aether");
+
+var bot = new Aether("Bot TOKEN");
+// Replace TOKEN with your bot account's token
+
+bot.on("ready", () => { // When the bot is ready
+    console.log("Ready!"); // Log "Ready!"
+});
+
+bot.on("error", (err) => {
+  console.error(err); // or your preferred logger
+});
+
+bot.on("messageCreate", (msg) => { // When a message is created
+    if(msg.content === "!ping") { // If the message content is "!ping"
+        bot.createMessage(msg.channel.id, "Pong!");
+        // Send a message in the same channel with "Pong!"
+    } else if(msg.content === "!pong") { // Otherwise, if the message is "!pong"
+        bot.createMessage(msg.channel.id, "Ping!");
+        // Respond with "Ping!"
+    }
+});
+
+bot.connect(); // Get the bot to connect to Discord
+```
+
+
+Useful Links
+------------
+
+- [The website](https://jxcksondev.netlify.app/Aether) has more details and documentation.
+- [The GitHub repo](https://github.com/jxckson-ctrl/Aether-library) is where development primarily happens.
+- [The NPM package webpage](https://npmjs.com/package/aether-library) is, well, the webpage for the NPM package.
+
+License
+-------
+
+Refer to the [LICENSE](LICENSE) file.
+
+Copyright Aether 2021
--- index.js
+++ index.js
@@ -1,8 +1,51 @@
 
 
-
-
+const Client = require('./lib/Client')
 
 function Aether(token, options) {
     return new Client(token, options);
 }
+
+Aether.Base = require('./lib/Structures/Base')
+Aether.Bucket = require('./lib/util/Bucket') 
+Aether.Call = require("./lib/structures/Call");
+Aether.CategoryChannel = require("./lib/structures/CategoryChannel");
+Aether.Channel = require("./lib/structures/Channel");
+Aether.Client = Client;
+Aether.Collection = require('./lib/util/Collection')
+Aether.Command = require("./lib/command/Command");
+Aether.CommandClient = require("./lib/command/CommandClient");
+Aether.Constants = require('./lib/Constants')
+Aether.DiscordHTTPError = require("./lib/errors/DiscordHTTPError");
+Aether.DiscordRESTError = require("./lib/errors/DiscordRESTError");
+Aether.ExtendedUser = require("./lib/structures/ExtendedUser");
+Aether.GroupChannel =  require("./lib/structures/GroupChannel");
+Aether.Guild = require("./lib/structures/Guild");
+Aether.GuildChannel = require("./lib/structures/GuildChannel");
+Aether.GuildIntegration = require("./lib/structures/GuildIntegration");
+Aether.GuildPreview = require("./lib/structures/GuildPreview");
+Aether.GuildTemplate = require("./lib/structures/GuildTemplate");
+Aether.Invite = require("./lib/structures/Invite");
+Aether.Member = require("./lib/structures/Member");
+Aether.Message = require("./lib/structures/Message");
+Aether.NewsChannel = require("./lib/structures/NewsChannel");
+Aether.Permission = require("./lib/structures/Permission");
+Aether.PermissionOverwrite = require("./lib/structures/PermissionOverwrite");
+Aether.PrivateChannel = require("./lib/structures/PrivateChannel");
+Aether.Relationship = require("./lib/structures/Relationship");
+Aether.RequestHandler = require("./lib/rest/RequestHandler");
+Aether.Role = require("./lib/structures/Role");
+Aether.SequentialBucket = require("./lib/util/SequentialBucket");
+Aether.Shard = require("./lib/gateway/Shard");
+Aether.SharedStream = require("./lib/voice/SharedStream");
+Aether.StoreChannel = require("./lib/structures/StoreChannel");
+Aether.TextChannel = require("./lib/structures/TextChannel");
+Aether.UnavailableGuild = require("./lib/structures/UnavailableGuild");
+Aether.User = require("./lib/structures/User");
+Aether.VERSION = require("./package.json").version;
+Aether.VoiceChannel = require("./lib/structures/VoiceChannel");
+Aether.VoiceConnection = require("./lib/voice/VoiceConnection");
+Aether.VoiceConnectionManager = require("./lib/voice/VoiceConnectionManager");
+Aether.VoiceState = require("./lib/structures/VoiceState");
+
+module.exports = Aether;
